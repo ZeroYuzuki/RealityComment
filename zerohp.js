1bce6b8da1ccaf59123580ee9ce2ab20bc003e53
ZeroYuzuki <86774447+ZeroYuzuki@users.noreply.github.com>
2022-04-11 19:30:59 +0900

    Update zerohp.js

--- zerohp.js
+++ zerohp.js
@@ -1,38 +1,62 @@
-document.getElementById("text-button").onclick = function() {
-  document.getElementById("text").innerHTML = "クリックされた！";
-};
 
-// カウントダウンする秒数
- var sec = 180;
+ // 設定開始（メッセージの内容を設定してください）
 
- // 開始日時を設定
- var dt = new Date();
- console.log("Start: ", dt);
- // 終了時刻を開始日時+カウントダウンする秒数に設定
- var endDt = new Date(dt.getTime() + sec * 1000);
- console.log("End : ", endDt);
+var msg1 = 'おはよう！'; // 時間帯1
+var msg2 = '午前中はやる気出ないです。。'; // 時間帯2
+var msg3 = 'お昼はもう食べた？'; // 時間帯3
+var msg4 = 'とりあえず午後も頑張りますか'; // 時間帯4
+var msg5 = '風呂入ったかー？'; // 時間帯5
+var msg6 = 'まだ起きてるの？'; // 時間帯6
 
- // 1秒おきにカウントダウン
- var cnt = sec;
- var id = setInterval(function(){
-   cnt--;
-   console.log(cnt);
-   // 現在日時と終了日時を比較
-   dt = new Date();
-   if(dt.getTime() >= endDt.getTime()){
-     clearInterval(id);
-     window.alert("3分経ったよ！");
-   }
- }, 1000);
+// 設定終了
 
-function msgdsp() {
-          alert("ピンク色のリンク押してもらえるとコメ欄専用ページに飛べるよ");
-      }
 
-      function botan() {
-                alert("天気ってググっとけ");
-            }
+var now = new Date();
 
-            function photo() {
-                      alert("右にあるやん？ちゃんとみぃや？");
-                  }
+var hour = now.getHours();
+
+
+// 表示開始
+
+// 時間帯1
+if(hour >= 5 && hour <= 8){
+
+	document.write(msg1);
+
+}
+// 時間帯2
+else if(hour >= 9 && hour <= 11){
+
+	document.write(msg2);
+
+}
+// 時間帯3
+else if(hour >= 12 && hour <= 13){
+
+	document.write(msg3);
+
+}
+// 時間帯4
+else if(hour >= 14 && hour <= 17){
+
+	document.write(msg4);
+
+}
+// 時間帯5
+else if(hour >= 18 && hour <= 23){
+
+	document.write(msg5);
+
+}
+// 時間帯6
+else{
+
+	document.write(msg6);
+
+}
+
+// 表示終了
+
+// -->
+
+
